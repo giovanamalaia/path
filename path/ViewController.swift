e99647e5bb96cd7cfcf396ceac47e4b8c2616c9d
Giovana Malaia <giovanamalaia@hotmail.com>
2025-02-07 16:10:18 -0300

    Initial Commit

---
+++ path/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  path
+//
+//  Created by Giovana Malaia on 07/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pathTests/pathTests.swift
@@ -0,0 +1,17 @@
+//
+//  pathTests.swift
+//  pathTests
+//
+//  Created by Giovana Malaia on 07/02/25.
+//
+
+import Testing
+@testable import path
+
+struct pathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
