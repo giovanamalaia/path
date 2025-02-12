f0cfcce7f54a0cbada96fd0fd63c4f45c8427b15
Giovana Malaia <giovanamalaia@hotmail.com>
2025-02-12 16:29:46 -0300

    TimelineCellView adicionada na TableView

--- path/ViewController.swift
+++ path/ViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
 
@@ -17,38 +19,67 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        let xib = UINib(nibName: "TimelineSongCellView", bundle: nil)
-        tableView.register(xib, forCellReuseIdentifier: "TimelineSongCellView")
+        let songXib = UINib(nibName: "TimelineSongCellView", bundle: nil)
+        tableView.register(songXib, forCellReuseIdentifier: "TimelineSongCellView")
+        
+//        let imageXib = UINib(nibName: "TimelineImageCellView", bundle: nil)
+//        tableView.register(imageXib, forCellReuseIdentifier: "TimelineImageCellView")
+        
+        let defaultXib = UINib(nibName: "TimelineCellView", bundle: nil)
+        tableView.register(defaultXib, forCellReuseIdentifier: "TimelineCellView")
     }
-}
-
-// MARK: - UITableViewDataSource
-extension ViewController: UITableViewDataSource {
     
+    // escolher aleatoriamente um tipo de célula
+    func randomCellType() -> String {
+        let cellTypes = ["TimelineSongCellView", "TimelineCellView"]
+        return cellTypes.randomElement() ?? "TimelineCellView"
+    }
+
+    // MARK: - UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return 10
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineSongCellView", for: indexPath) as? TimelineSongCellView else {
+        let cellType = randomCellType()
+
+        switch cellType {
+        case "TimelineSongCellView":
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineSongCellView", for: indexPath) as? TimelineSongCellView else {
+                return UITableViewCell()
+            }
+            cell.profilePicture.image = UIImage(systemName: "circle.fill")
+            cell.albumCoverPicture.image = UIImage(systemName: "square.fill")
+            return cell
+            
+//        case "TimelineImageCellView":
+//            guard let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineImageCellView", for: indexPath) as? TimelineImageCellView else {
+//                return UITableViewCell()
+//            }
+//            return cell
+            
+        case "TimelineCellView":
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineCellView", for: indexPath) as? TimelineCellView else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        default:
             return UITableViewCell()
         }
-
-        return cell
     }
-}
-
-// MARK: - UITableViewDelegate
-extension ViewController: UITableViewDelegate {
+    
+    // MARK: - UITableViewDelegate
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         print("Célula \(indexPath.row) selecionada")
     }
 }
+
 
 
