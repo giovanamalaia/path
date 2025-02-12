e73d73b4d4f0c6358a087b500bb4baafcc0c5a4e
Giovana Malaia <giovanamalaia@hotmail.com>
2025-02-12 14:52:09 -0300

    Adicionei a TimelineSongCellView na TableView

--- path/ViewController.swift
+++ path/ViewController.swift
@@ -7,31 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.delegate = self
         tableView.dataSource = self
-        let xib = UINib(nibName: "TableViewCell", bundle: nil)
-        tableView.register(xib, forCellReuseIdentifier: "cell")
+        
+        let xib = UINib(nibName: "TimelineSongCellView", bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: "TimelineSongCellView")
     }
-    
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        20
+        return 20
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineSongCellView", for: indexPath) as? TimelineSongCellView else {
+            return UITableViewCell()
+        }
+
         return cell
     }
-    
-
-   
-
-
 }
+
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("Célula \(indexPath.row) selecionada")
+    }
+}
+
 
