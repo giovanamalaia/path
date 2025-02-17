ddd97ac43635400890ed5351d692b65771440897
Giovana Malaia <giovanamalaia@hotmail.com>
2025-02-17 17:40:08 -0300

    TableView em PeoplePostView - PersonCell adicionada na TableView - PersonModel criado (+ lista de contatos)

--- path/Controller/PeoplePostView.swift
+++ path/Controller/PeoplePostView.swift
@@ -8,6 +8,55 @@
 import Foundation
 import UIKit
 
-class PeoplePostView: UIViewController {
+class PeoplePostView: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
+    @IBOutlet weak var searchBarPeople: UISearchBar!
+    @IBOutlet weak var tableViewPeople: UITableView!
+    
+    var filteredContactList: [Person] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableViewPeople.delegate = self
+        tableViewPeople.dataSource = self
+        searchBarPeople.delegate = self
+        
+        let personXib = UINib(nibName: "PersonCellView", bundle: nil)
+        tableViewPeople.register(personXib, forCellReuseIdentifier: "PersonCellView")
+        
+        filteredContactList = PeopleData.peopleList 
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredContactList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCellView", for: indexPath) as? PersonCellView else {
+            return UITableViewCell()
+        }
+        
+        let person = filteredContactList[indexPath.row]
+        cell.contactName.text = person.name
+        
+        return cell
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredContactList = PeopleData.peopleList
+        } else {
+            filteredContactList = PeopleData.peopleList.filter { person in
+                person.name.lowercased().contains(searchText.lowercased())
+            }
+        }
+        tableViewPeople.reloadData()
+    }
 }
---
+++ path/Model/PersonModel.swift
@@ -0,0 +1,43 @@
+//
+//  PersonModel.swift
+//  path
+//
+//  Created by Giovana Malaia on 17/02/25.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+}
+
+struct PeopleData {
+    static let peopleList: [Person] = [
+        Person(name: "Albert Einstein"),
+        Person(name: "Benjamin Lewis"),
+        Person(name: "Beyoncé"),
+        Person(name: "Emily Davis"),
+        Person(name: "Emma Stone"),
+        Person(name: "Grace Hopper"),
+        Person(name: "Harry Styles"),
+        Person(name: "Hermione Granger"),
+        Person(name: "Jack Sparrow"),
+        Person(name: "James Wilson"),
+        Person(name: "Liam Thompson"),
+        Person(name: "Madison White"),
+        Person(name: "Michael Carter"),
+        Person(name: "Mona Lisa"),
+        Person(name: "Olivia Johnson"),
+        Person(name: "Paul McCartney"),
+        Person(name: "Peter Parker"),
+        Person(name: "Rachel Green"),
+        Person(name: "Scarlett Johansson"),
+        Person(name: "Sherlock Holmes"),
+        Person(name: "Sophia Anderson"),
+        Person(name: "Steve Jobs"),
+        Person(name: "Taylor Swift"),
+        Person(name: "Tony Stark")
+    ]
+}
+
+
