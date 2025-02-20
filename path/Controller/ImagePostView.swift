9ff95753c83e03bf6dc638a76b1318c8b9b41d21
Giovana Malaia <giovanamalaia@hotmail.com>
2025-02-20 20:35:59 -0300

    Navigation ImagePostView

---
+++ path/Controller/ImagePostView.swift
@@ -0,0 +1,28 @@
+//
+//  ImagePostView.swift
+//  path
+//
+//  Created by Giovana Malaia on 20/02/25.
+//
+
+import Foundation
+import UIKit
+
+class ImagePostViewController: UIViewController {
+    @IBOutlet var imagePost: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(navigateToWritePostView))
+        imagePost.addGestureRecognizer(tapGesture)
+        imagePost.isUserInteractionEnabled = true
+    }
+
+    @objc func navigateToWritePostView() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let writePostVC = storyboard.instantiateViewController(withIdentifier: "WritePostView") as? WritePostView {
+            navigationController?.pushViewController(writePostVC, animated: true)
+        }
+    }
+}
